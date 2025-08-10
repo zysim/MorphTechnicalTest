8b227365ed90928c3ab7945178d8663a450322fd
zysim <emailmeloh@gmail.com>
2025-08-10 16:57:29 +0800

    Drop unused routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,12 +10,6 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::post('mine', function () {
-    return 'Yippee';
-});
-
-Route::resource('test', TestController::class);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
